1fb69f26022f74210414d27b4af0fed61d8f22a2
edwinss <edwinwakanda@gmail.com>
2026-07-27 11:36:40 +0700

    l

--- app/lib/api-url.ts
+++ app/lib/api-url.ts
@@ -1,6 +1,4 @@
-// Mengambil URL dari Environment Variable Vercel / .env
-// Jika tidak ada, otomatis menggunakan fallback URL backend Vercel kamu
-const ENV_URL = process.env.NEXT_PUBLIC_API_BASE_URL || process.env.NEXT_PUBLIC_API_URL; //
+const ENV_URL = process.env.NEXT_PUBLIC_API_BASE_URL || process.env.NEXT_PUBLIC_API_URL;
 
 export const API_BASE_URL = 
   ENV_URL && ENV_URL.trim() !== '' 
